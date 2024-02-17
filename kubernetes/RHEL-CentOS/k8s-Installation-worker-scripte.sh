761f830b4f8d9c2d67ac91f697f436a65985e455
Hassan Eid <105084413+Hassan-Eid-Hassan@users.noreply.github.com>
2024-02-18 01:26:24 +0200

    Update k8s-Installation-worker-scripte.sh

--- kubernetes/RHEL-CentOS/k8s-Installation-worker-scripte.sh
+++ kubernetes/RHEL-CentOS/k8s-Installation-worker-scripte.sh
@@ -116,7 +116,7 @@ cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 name=Kubernetes
 baseurl=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/
 enabled=1
-gpgcheck=1
+gpgcheck=0
 gpgkey=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/repodata/repomd.xml.key
 exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
 EOF
